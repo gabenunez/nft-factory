0c148c74447b65d33a9749796249a1a264bee83f
Caleb Bishop <calebbishopcs@gmail.com>
2021-09-05 16:43:42 -0400

    Separated image generation into seperate functions, changed path to /preview, returns base64 encoded image

--- server/src/app.py
+++ server/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request, send_file
+from flask import Flask,request, send_file, jsonify
 import json
 import tempfile
 import os
@@ -6,64 +6,87 @@ from PIL import Image
 from collections import OrderedDict
 from random import randint
 import io
+import base64
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello,World!</p>"
 
-@app.route("/upload",methods=['GET','POST'])
-def upload_files():
+def create_pool(files,request_data):
+    pool = OrderedDict()
+    layers = request_data['layers']
+    for layer in layers:
+        pool[layer] = {}
+    for file in files:
+        file_name = file.filename
+        img_attr = request_data['data'][file_name]
+        layer = img_attr['layer']
+        name = img_attr['name']
+        rarity = img_attr['percent']
+        if not rarity in pool[layer]:
+            pool[layer].update({
+                rarity: []
+            })
+        pool[layer][rarity].append({name:file})
+    return pool
+
+def calculate_rarity(rarity_pool):
+    selected_rarity = 1000
+    chance = randint(0,100)
+    for rarity in rarity_pool:
+        t1 = 0 + rarity
+        t2 = 100 - rarity
+        if chance <= t1 or chance >= t2:
+            if rarity < selected_rarity:
+                selected_rarity = rarity
+    if selected_rarity == 1000:
+        selected_rarity = rarity_pool[randint(0,len(rarity_pool)-1)]
+    
+    return selected_rarity
+
+def pool_random_image(pool):
+    in_img = None
+    buf = io.BytesIO()
+
+    for layer in pool:
+        rarity_pool = list(pool[layer])
+        selected_rarity = calculate_rarity(rarity_pool)  
+        item_pool = pool[layer][selected_rarity]
+        item = item_pool[randint(0,len(item_pool)-1)]
+        file = item[list(item)[0]]
+        if not in_img:
+            in_img = Image.open(file).convert('RGBA')
+        else:
+            image = Image.open(file).convert('RGBA')
+            in_img.paste(image,(0,0),image)
+    
+    in_img.save(buf,'PNG')
+    buf.seek(0)
+    return buf.read()
+
+def generate_image(files,request_data):
+    return pool_random_image(create_pool(files,request_data))
+
+
+def encode_image(img_bytes):
+    return f"data:image/png;base64,{base64.b64encode(img_bytes).decode('ascii')}"
+
+@app.route("/generate",methods=['POST'])
+def generate_bulk_data():
+    pass
+
+@app.route("/preview",methods=['GET','POST'])
+def generate_preview():
     if request.method == 'POST':
         upload_files = request.files.getlist("files")
         request_data = json.loads(request.form.get('filedata'))
-        layers = request_data['layers']
-
-        pool = OrderedDict()
-
-        for layer in layers:
-            pool[layer] = {}
-
-        for file in upload_files:
-            file_name = file.filename
-            img_attr = request_data['data'][file_name]
-            layer = img_attr['layer']
-            name = img_attr['name']
-            rarity = img_attr['percent']
-            if not rarity in pool[layer]:
-                pool[layer].update({
-                    rarity: []
-                })
-            pool[layer][rarity].append({name:file})
-        
-        in_img = None
-        for layer in pool:
-            chance = randint(0,100)
-            rarity_pool = list(pool[layer])
-            print(rarity_pool)
-            selected_rarity = 1000
-            for rarity in rarity_pool:
-                t1 = 0 + rarity
-                t2 = 100 - rarity
-                if chance <= t1 or chance >= t2:
-                    if rarity < selected_rarity:
-                        selected_rarity = rarity
-                
-            item_pool = pool[layer][selected_rarity]
-            item = item_pool[randint(0,len(item_pool)-1)]
-            file = item[list(item)[0]]
-            if not in_img:
-                in_img = Image.open(file).convert('RGBA')
-            else:
-                image = Image.open(file).convert('RGBA')
-                in_img.paste(image,(0,0),image)   
-        buf = io.BytesIO()
-        #in_img = in_img.resize((512,512),Image.NEAREST)
-        in_img.save(buf,'PNG')
-        buf.seek(0)
-        byteImg = buf.read()
-        
-        return send_file(io.BytesIO(byteImg),mimetype='image/png',attachment_filename='generated.png')
+        image_bytes = generate_image(upload_files,request_data)
+        encoded = encode_image(image_bytes)
+        res_data = {
+            "image":encoded
+        }
+        return jsonify(res_data)
     
     #For testing through local browser
     return '''
