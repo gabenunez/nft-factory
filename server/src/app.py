196fec8e714794c00004f9ad8879ebb22ccf7df4
Caleb Bishop <calebbishopcs@gmail.com>
2021-09-07 16:14:17 -0400

    Updated app.py, updated preview to post, removed hello world

--- server/src/app.py
+++ server/src/app.py
@@ -3,21 +3,8 @@ from module import image_factory as imgfactory
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello,World!</p>"
-
-@app.route("/preview",methods=['GET','POST'])
+@app.route("/preview",methods=['POST'])
 def generate_preview():
     if request.method == 'POST':
         return imgfactory.preview_image(request)
-    
-    #For testing through local browser
-    return '''
-    <form method="POST" enctype="multipart/form-data" action="/upload">
-        <input type="file" name="files" multiple="">
-        <input type="text" name="filedata">
-        <input type="submit" value="upload">
-    </form>
-    '''
 
