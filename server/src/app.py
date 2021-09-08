bf690aef5537d89282f189bbb1eeecc3cd44fe28
Caleb Bishop <calebbishopcs@gmail.com>
2021-09-08 18:04:44 -0400

    Added zip file generation, added new endpoint, added amount to be generated for param

--- server/src/app.py
+++ server/src/app.py
@@ -3,8 +3,33 @@ from module import image_factory as imgfactory
 
 app = Flask(__name__)
 
+@app.route("/health",methods=['GET'])
+def health():
+    return "Healthy"
+
 @app.route("/preview",methods=['POST'])
-def generate_preview():
+def create_preview():
     if request.method == 'POST':
+        if not request.json:
+            return "Invalid JSON"
         return imgfactory.preview_image(request)
+
+@app.route("/create_collectible",methods=['POST'])
+def create_collectible():
+    if request.method == 'POST':
+        if not request.json:
+            return "Invalid JSON"
+        
+        data = request.args
+
+
+        if 'n' in data:
+            amount = request.args.get('n',type=int)          
+            zipped_bytes = imgfactory.create_images(request,amount)
+            return send_file(zipped_bytes,attachment_filename='generated.zip',as_attachment=True)
+        else:
+            return "Invalid(Missing Params)"
+        
+
+    return "Invalid"
 
