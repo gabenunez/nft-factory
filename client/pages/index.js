e10ba2a4cf0d68a98beb14e0e58340aa058a77d9
Gabe Nuñez <gabe@gabenunez.com>
2021-09-08 21:30:54 -0700

    Save zip files

--- client/pages/index.js
+++ client/pages/index.js
@@ -10,33 +10,62 @@ import Layers from '../components/layers';
 import Button from 'react-bootstrap/Button';
 import { BsQuestionOctagon } from 'react-icons/bs';
 import FileToBase64 from 'dd-file-to-base64';
+import { saveAs } from 'file-saver';
 
 export default function Home() {
   const [selectedImages, setSelectedImages] = useState([{ name: '', images: [] }]);
   const [previewImage, setPreviewImage] = useState('');
 
-  async function fetchExampleImage() {
-    try {
-      const requestPayload = {};
+  async function createImagePayload() {
+    const requestPayload = {};
 
-      const reversedLayers = [...selectedImages].reverse();
-      for (const layer of reversedLayers) {
-        let base64images = layer.images.map((image) => {
-          return FileToBase64.convert(image);
-        });
+    const reversedLayers = [...selectedImages].reverse();
+    for (const layer of reversedLayers) {
+      let base64images = layer.images.map((image) => {
+        return FileToBase64.convert(image);
+      });
 
+      try {
         base64images = await Promise.all(base64images);
-
-        requestPayload[layer.name] = layer.images.map((image, imageIndex) => {
-          return {
-            name: image.attributeName,
-            image: base64images[imageIndex].split('base64,')[1],
-            chance: image.percentageChance
-          };
-        });
+      } catch (error) {
+        alert('Wow. This is weird. We had a problem converting your images. Please try again.');
+        throw new Error(error);
       }
 
+      requestPayload[layer.name] = layer.images.map((image, imageIndex) => {
+        return {
+          name: image.attributeName,
+          image: base64images[imageIndex].split('base64,')[1],
+          chance: image.percentageChance
+        };
+      });
+
+      return requestPayload;
+    }
+  }
+
+  async function generateImageCollection() {
+    try {
+      const requestPayload = await createImagePayload();
+      const response = await axios.post('/api/create_collectible?n=27', requestPayload, {
+        responseType: 'arraybuffer'
+      });
+
+      const zipBlob = new Blob([response.data]);
+      saveAs(zipBlob, 'collection.zip');
+    } catch (err) {
+      alert(
+        "Sorry, we're having problems connecting to our internal server. Please try again later."
+      );
+      console.error(err);
+    }
+  }
+
+  async function fetchExampleImage() {
+    try {
+      const requestPayload = await createImagePayload();
       const response = await axios.post('/api/preview', requestPayload);
+
       setPreviewImage(response.data.image);
     } catch (err) {
       alert(
@@ -45,6 +74,7 @@ export default function Home() {
       console.error(err);
     }
   }
+
   return (
     <div>
       <Head>
@@ -65,8 +95,12 @@ export default function Home() {
                 {!previewImage && <BsQuestionOctagon color="#0d6efd" size="100px" />}
               </div>
 
-              <div className="d-flex justify-content-center">
+              <div className="d-flex justify-content-center mb-3">
                 <Button onClick={fetchExampleImage}>Generate example image</Button>
+              </div>
+
+              <div className="d-flex justify-content-center">
+                <Button onClick={generateImageCollection}>Generate image collection</Button>
               </div>
             </Col>
             <Col md={9}>
